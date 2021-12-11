25a2ac5e274fdadbab53dccd30bd1edccf1dfbe1
orefice-michele <91534750+orefice-michele@users.noreply.github.com>
2021-12-11 14:19:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lin.h"
+using namespace std;
 
 int main(){
+  int pos;
+  char a[10][20], f[1][20];
+  for(int i=0, i<10; i++){
+    cin>>a[i];
+  }
+  cin>>f[0];
+  
+  
+  if(ricerca(a, f, pos)){
+    cout << pos << endl;
+  }else{
+    cout << "non presente"<<endl;
+  }
   return 0;
 }
